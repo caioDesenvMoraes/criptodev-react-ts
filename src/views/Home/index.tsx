55c5639a81ed6ebd2171998a67ee99b8e0894ca8
Caio Moraes <caio.moraes.fbp@gmail.com>
2022-04-15 11:57:37 -0300

    update interfaces

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -4,24 +4,35 @@ import NavBar from "../../Components/NavBar"
 import Footer from "../../Components/Footer"
 
 import { apiData } from "../../services/apiData"
-import { IBlockChainData } from "../../Interfaces"
+import { IStore } from "../../Interfaces"
 
 import Constants from "../../Constants"
 
 // import styled from "./style"
 
 const Home: React.FC = () => {
-    const [ data, setData ] = useState<IBlockChainData>( {} as IBlockChainData )
+    const [ data, setData ] = useState<IStore[]>( [] )
 
     useEffect( () => {
-        apiData.get("stores", ).then( res => {
-            console.log(res.data)
+        apiData.get("stores").then( res => {
+            setData( res.data )
         })
     }, [])
     return (
         <>
             <NavBar/>
-            <h1>Home Page</h1>
+            <h1>Listando Lojas</h1>
+            {data.map( (el, index) => (
+                <>
+                    <div key={ index }>
+                        <h2>{ el.label }</h2>
+                        <img src={ el.logo } alt={ el.label } width={ 300 } height={ "auto" }/>
+                        <p>{ el.address.street }</p>
+                        <p>{ el.address.city }</p>
+                        <p>{ el.address.state }</p>
+                    </div>
+                </>
+            ) )}
             <Footer/>
         </>
     )
