1f7e94236ab599fd70c8909c04b886c3bac1aead
Caio Moraes <caio.moraes.fbp@gmail.com>
2022-04-15 12:29:22 -0300

    end project

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -6,33 +6,46 @@ import Footer from "../../Components/Footer"
 import { apiData } from "../../services/apiData"
 import { IStore } from "../../Interfaces"
 
-import Constants from "../../Constants"
-
-// import styled from "./style"
+import styles from "./Home.module.css"
 
 const Home: React.FC = () => {
     const [ data, setData ] = useState<IStore[]>( [] )
+    const [ isLoad, setIsLoad ] = useState<boolean>( false )
 
     useEffect( () => {
+        setIsLoad(true)
         apiData.get("stores").then( res => {
             setData( res.data )
-        })
+        }).finally( () => setIsLoad(false))
     }, [])
     return (
         <>
             <NavBar/>
-            <h1>Listando Lojas</h1>
-            {data.map( (el, index) => (
-                <>
-                    <div key={ index }>
-                        <h2>{ el.label }</h2>
-                        <img src={ el.logo } alt={ el.label } width={ 300 } height={ "auto" }/>
-                        <p>{ el.address.street }</p>
-                        <p>{ el.address.city }</p>
-                        <p>{ el.address.state }</p>
-                    </div>
-                </>
-            ) )}
+            <div className={ styles.container }>
+                <h1>Listando Lojas</h1>
+                { isLoad ? (
+                    <>
+                        <div>
+                            <h3>Aguarde Carregando...</h3>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        {data.map( (el, index) => (
+                            <div className={ styles.storeLists }>
+                                <div key={ index }>
+                                    <h2>{ el.label }</h2>
+                                    <img src={ el.logo } alt={ el.label } width={ 300 } height={ "auto" }/>
+                                    <p>{ el.address.street }</p>
+                                    <p>{ el.address.city }</p>
+                                    <p>{ el.address.state }</p>
+                                </div>
+                            </div>
+                        ))}
+
+                    </>
+                )}
+            </div>
             <Footer/>
         </>
     )
