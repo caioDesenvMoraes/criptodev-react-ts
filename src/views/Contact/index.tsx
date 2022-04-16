0b4c45e7e01f21e01f153280e749907171b114b5
Caio Moraes <caio.moraes.fbp@gmail.com>
2022-04-16 19:11:22 -0300

    create contact

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import NavBar from "../../Components/NavBar"
 import Footer from "../../Components/Footer"
 
-// import styled from "./style"
+import styles from "./Contact.module.css"
 
 const Contact: React.FC = () => {
     return(
         <>
             <NavBar/>
-            <h1>Contact Page</h1>
+            <div className={ styles.containerContact}>
+                <h1>Deixe seu contato</h1>
+                <form className={ styles.form }>
+                    <input type="text" className={ styles.item } name="name" id="name" placeholder="Informe seu nome"/>
+                    <input type="number" className={ styles.item } name="telefone" id="telefone" placeholder="Informe seu telefone"/>
+                    <input type="email" className={ styles.item } name="email" id="email" placeholder="Informe seu e-mail"/>
+                    <input type="submit" className={ styles.button } value="Enviar"/>
+                </form>
+            </div>
             <Footer/>
         </>
     )
